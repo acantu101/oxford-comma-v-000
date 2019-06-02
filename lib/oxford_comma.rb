21a4c5ca3bdf56eb3402a7a2fb8fa2e2b122bfd2
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-02 19:06:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,6 @@
 def oxford_comma(array)
 if array.count == 0 then array.join
 elsif array.count == 2 then array.join(" and ")
-elsif array.count == 3 then array.join(', ') + " and " + array[-1]
+elsif array.count == 3 then array.join(', ') + " and " + array[3]
 end
 end
