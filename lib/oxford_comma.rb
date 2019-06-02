d44e87451c98f65c8d3088eb1e48b4a23ca5cf87
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-02 18:19:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,7 @@
-def oxford_comma(array)
+#string to an array, an array into a string, or a range into an array
+#that takes an argument array of string elements and converts it into a string using the Oxford comma. 
 
+
+def oxford_comma(array)
+array.join(",")
 end
