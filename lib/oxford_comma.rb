2302f4f92f72b5e92a0a2489c7873f6d95604238
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-02 18:52:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,9 @@
 #that takes an argument array of string elements and converts it into a string using the Oxford comma.
 
 
-def oxford_comma(array)
-if array 0 then array.join
-elsif array 2 then array.join(" and ")
-elsif array 3 then array.to_s
+def oxford_comma(Array)
+if Array 0 then array.join
+elsif Array 2 then array.join(" and ")
+elsif Array 3 then array.to_s
 end
 end
