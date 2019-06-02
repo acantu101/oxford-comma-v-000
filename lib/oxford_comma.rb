a91e3e01b14b1ea7bb8180e6a19fc774f9e8e83f
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-02 18:36:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,5 +4,6 @@
 
 def oxford_comma(array)
 return array.join(", ") if array[0-1]
-return array[0..-2].join(", ") + ", and " + array[-1] if array[0..-2]
+elsif array[0..2]
+  return array[0..-2].join(", ") + ", and " + array[-1]
 end
