a053087d74e6887ebeca11be8676f6ab15439d37
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-02 19:25:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
 if array.count == 1 then array.join
 elsif array.count == 2 then array.join(" and ")
 elsif array.count >= 3
-   array[-1]<< "and"
+   array[-2]<< "and"
    array.join(', ')
 end
 end
