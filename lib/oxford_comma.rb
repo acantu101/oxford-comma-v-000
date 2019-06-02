17b7d8f8fa3f5ae00fd14caacc4a5072f54e280d
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-02 18:43:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,7 @@
 def oxford_comma(array)
 if array == 0 then array.join(" and ")
 elsif array == 2 then array.join(", ")
-else array.join(", ") + ", and " 
+elsif array == [3]
+  then array.join(", ") + ", and " + array[-1]
 end
 end
