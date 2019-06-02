025b3b7f90ac1ca85b515b0f9f9869dc0936eaef
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-02 18:53:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,8 @@
 
 
 def oxford_comma(Array)
-if Array 0 then array.join
-elsif Array 2 then array.join(" and ")
-elsif Array 3 then array.to_s
+if array.lenght 0 then array.join
+elsif array 2 then array.join(" and ")
+elsif array 3 then array.to_s
 end
 end
