808902dccca718a5b5a4f2b6845b6593bc38b531
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-02 18:19:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 #string to an array, an array into a string, or a range into an array
-#that takes an argument array of string elements and converts it into a string using the Oxford comma. 
+#that takes an argument array of string elements and converts it into a string using the Oxford comma.
 
 
 def oxford_comma(array)
